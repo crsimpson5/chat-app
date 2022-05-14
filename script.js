76c37b0334c357492641fe7d20d4f7ba24305b2f
Curtis Simpson <crsimpson5@gmail.com>
2022-05-13 20:32:21 -0500

    Add prefers-reduced-motion support

--- script.js
+++ script.js
@@ -7,7 +7,18 @@ function loadMessage(el, delay) {
   }, delay);
 }
 
-loadMessage(messages.shift(), 1000);
-loadMessage(messages.shift(), 3000);
-loadMessage(messages.shift(), 4500);
-loadMessage(messages.shift(), 4750);
+function animate() {
+  if (window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    messages.forEach((el) => {
+      el.classList.add("active");
+      el.style.maxHeight = el.scrollHeight + "px";
+    });
+  } else {
+    loadMessage(messages.shift(), 1000);
+    loadMessage(messages.shift(), 3000);
+    loadMessage(messages.shift(), 4500);
+    loadMessage(messages.shift(), 4750);
+  }
+}
+
+animate();
